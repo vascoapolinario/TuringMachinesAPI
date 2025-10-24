3632f46bc34856f6926c75baa1de6e06ed420d32
Vasco Apolinário <vascoapolinario@gmail.com>
2025-10-24 13:23:31 +0100

    Healthchecks to keep render up

---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuringMachinesAPI.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [HttpHead]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+    }
+}
